3608f43dd4dfa716a46911702cabe89741dc9627
Grace Williams <williamsg23@mail.wlu.edu>
2022-10-31 16:06:32 -0400

    more on map pop up

--- map.R
+++ map.R
@@ -6,12 +6,12 @@ leaflet(data=finalDogData) %>%
     lng = ~Longitude,
     lat = ~Latitude,
     label = finalDogData$Breed,
-    popup = ~paste(
+    popup = ~paste("<p><b>" , finalDogData$Breed, "</b></p>" , 
+                   "<p>", "Origin =", finalDogData$origin, "</p>" , 
+                   "<p>", "Group =", finalDogData$Group, "</p>",
       '<img src="' , finalDogData$imageURL,'" 
       width=200
       height=200
       />'), 
 )
-
-
 
