9d28e1cf573d3e975ec73412f488171e2eb0bcb3
Grace Williams <williamsg23@mail.wlu.edu>
2022-10-31 15:34:21 -0400

    adding images to the map function's popup

--- map.R
+++ map.R
@@ -1,6 +1,17 @@
 library(leaflet)
 
-leaflet(data=finalDogData) %>% addTiles() %>% addMarkers(popup = ~Breed) 
-#making HTMLs
+leaflet(data=finalDogData) %>% 
+  addTiles() %>% 
+  addMarkers(
+    lng = ~Longitude,
+    lat = ~Latitude,
+    label = finalDogData$Breed,
+    popup = ~paste(
+      '<img src="' , finalDogData$imageURL,'" 
+      width=200
+      height=200
+      />'), 
+)
 
-paste('<img src = ""', c("HTML"), '" />', sep = "")
+
+
