59d0c2d731d0bbaaec85620abf2dfd19dd0353bf
Jillian Rosenwasser <rosenwasserj23@mail.wlu.edu>
2022-11-02 15:47:45 -0400

    fixing x axis of plot

--- dropdown.R
+++ dropdown.R
@@ -1,2 +1,3 @@
 library(ggplot2)
-ggplot(finalDogData, aes(Year, Breed, col=Breed)) + geom_point()
+ggplot(finalDogData, aes(Year, Breed, col=Breed)) + geom_point() + scale_x_continuous(breaks = round(seq(min(finalDogData$Year), max(finalDogData$Year), by = 5)))
+
