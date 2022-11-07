f046eb8f499859a684b699ed45c8f724db110a9d
John Raymond <raymondj23@mail.wlu.edu>
2022-11-07 16:15:46 -0500

    finished plot with choices and title and cool theme

--- server.R
+++ server.R
@@ -2,6 +2,7 @@ library(shiny)
 library(leaflet)
 library(ggplot2)
 library(dplyr)
+library(ggthemes)
 
 # Define server logic required to draw a histogram
 function(input, output) {
@@ -30,7 +31,7 @@ function(input, output) {
       req(input$dogBreed)
       filter(finalDogData, Breed %in% input$dogBreed)
       })
-   ggplot(dogBreeds(), aes(Year, Breed)) + geom_point(show.legend = FALSE) + scale_x_continuous(breaks = round(seq(min(finalDogData$Year), max(finalDogData$Year), by = 10)))
+   ggplot(dogBreeds(), aes(Year, Breed)) + geom_point(show.legend = FALSE) + theme_hc() + ggtitle("TITLE") + scale_x_continuous(breaks = round(seq(min(finalDogData$Year), max(finalDogData$Year), by = 10)))
   })
 
     }
