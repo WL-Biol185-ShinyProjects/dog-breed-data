bf80dbb543e938e7a9e5ae8efc23d90c86147413
Jillian Rosenwasser <rosenwasserj23@mail.wlu.edu>
2022-11-09 15:47:56 -0500

    about page info

--- ui.R
+++ ui.R
@@ -39,9 +39,10 @@ navbarPage("Westminster Dog Show Data",
                     ,
            tabPanel("About",
                     titlePanel("Our story"),
-                    h4("learn about westminster"),
-                    p("hello"),
-                    "About information")
+                    h4("The Westminster Kennel Club organization began in 1877 with the mission of enhancing dogs’ lives, celebrating their companionship, promoting responsible dog ownership, and preserving breeds. The Westminster Kennel Club Dog Show is an all-breed conformation competition that began in 1948, making it the longest nationally televised like dog show.  "),
+                    h4("As dog lovers themselves, Grace, Mary Camilla, Jill, and John decided to bring to life a collection of data from both the American Kennel Club and the Westminster Kennel Club Dog Show.  "),
+                    h4("We hope you all enjoy exploring the visualizations as much as we enjoyed creating it!")
+                    )
            
     
 
