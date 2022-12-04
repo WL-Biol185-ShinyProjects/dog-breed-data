aea5148f3695d227ff7a8ff1475f0593b7f8dd8e
Grace Williams <williamsg23@mail.wlu.edu>
2022-12-04 18:23:07 -0500

    cleaning up shiny workspace to make it easier to read

--- server.R
+++ server.R
@@ -5,102 +5,139 @@ library(dplyr)
 library(ggthemes)
 library(DT)
 
-# Define server logic required to draw a histogram
+#DEFINING SERVER LOGIC 
+
 function(input, output) {
 
-  #CREATING A MAP
+  #CREATING A MAP WITH LIGHT AND DARK MODES
   
   output$map <- renderLeaflet({
       if(input$mapTheme=="Dark mode"){leaflet(data=finalDogData) %>%
+          
           addProviderTiles(provider = providers$NASAGIBS.ViirsEarthAtNight2012) %>% 
+          
           setView(0, 40, zoom =4) %>%
+          
           addMarkers(
-            lng = ~Longitude,
-            lat = ~Latitude,
-            label = finalDogData$Breed,
-            popup = ~paste("<p><b>" , finalDogData$Breed, "</b></p>" , 
-                           "<p>", "Origin =", finalDogData$origin, "</p>" , 
-                           "<p>", "Group =", finalDogData$Group, "</p>",
-                           '<img src="' , finalDogData$imageURL,'" 
-      width=200
-      height=200
-      />') 
+                      lng = ~Longitude,
+                      lat = ~Latitude,
+                      label = finalDogData$Breed,
+                      popup = ~paste("<p><b>" , finalDogData$Breed, "</b></p>" , 
+                                     "<p>", "Origin =", finalDogData$origin, "</p>" , 
+                                     "<p>", "Group =", finalDogData$Group, "</p>",
+                                     '<img src="' , finalDogData$imageURL,'" 
+                                     width=200
+                                     height=200
+                                       />') 
           )
+      
       } else {
+        
         leaflet(data=finalDogData) %>%
+          
           addTiles() %>% 
+          
           setView(0, 40, zoom =4) %>%
+          
           addMarkers(
-            lng = ~Longitude,
-            lat = ~Latitude,
-            label = finalDogData$Breed,
-            popup = ~paste("<p><b>" , finalDogData$Breed, "</b></p>" , 
-                           "<p>", "Origin =", finalDogData$origin, "</p>" , 
-                           "<p>", "Group =", finalDogData$Group, "</p>",
-                           '<img src="' , finalDogData$imageURL,'" 
-      width=200
-      height=200
+                      lng = ~Longitude,
+                      lat = ~Latitude,
+                      label = finalDogData$Breed,
+                      popup = ~paste("<p><b>" , finalDogData$Breed, "</b></p>" , 
+                                     "<p>", "Origin =", finalDogData$origin, "</p>" , 
+                                     "<p>", "Group =", finalDogData$Group, "</p>",
+                                     '<img src="' , finalDogData$imageURL,'" 
+                                     width=200
+                                     height=200
       />') 
           )
   }})
   
+  #CREATING A REACTIVE PLOT
+  
   output$plot <- renderPlot({
-    dogBreeds <- reactive({
-      req(input$dogBreed)
-      filter(finalDogData, Breed %in% input$dogBreed)
+      dogBreeds <- reactive({
+                      req(input$dogBreed)
+                      filter(finalDogData, Breed %in% input$dogBreed)
       })
-   ggplot(dogBreeds(), aes(Year, Breed,)) + geom_point(show.legend = FALSE, color = 'black') + theme_hc() + theme(plot.background = element_rect(fill = "#E6E6FA")) + scale_x_continuous(breaks = round(seq(min(finalDogData$Year), max(finalDogData$Year), by = 10)))
-   
+      
+      ggplot(dogBreeds(), 
+             aes(Year, Breed,)) + 
+             geom_point(show.legend = FALSE, color = 'black') + 
+             theme_hc() + 
+             theme(plot.background = element_rect(fill = "#E6E6FA")) + 
+             scale_x_continuous(breaks = round(seq(min(finalDogData$Year), max(finalDogData$Year), by = 10)))
   })
 
-  
+  #RENDERING THE DATA TABLE
   
   output$Dogs <- renderDataTable(downloadDogs, filter = 'top')
   
+  #INTERACTIVE DOWNLOAD BUTTON
+  
   output$downloadData <- downloadHandler(
+    
     filename = function() {
       paste("Dogs", Sys.Date(), ".csv", sep="")
     },
+    
     content = function (file) {
       write.csv (downloadDogs, file)
     }
   )
   
+  #CREATING FIRST INTERACTIVE PLOT
   
   output$Interactive <- renderPlot({
+    
     if(input$barData == "Origin"){
-      ggplot(breedWinStats, aes(Origin, Frequency)) + geom_col() + theme_hc() + theme(plot.background = element_rect(fill = "#E6E6FA")) + theme(axis.text.x = element_text(angle = 60, hjust = 1))
+      ggplot(breedWinStats, aes(Origin, Frequency)) + 
+            geom_col() + 
+            theme_hc() + 
+            theme(plot.background = element_rect(fill = "#E6E6FA")) + 
+            theme(axis.text.x = element_text(angle = 60, hjust = 1))
     }
     else {
       if(input$barData == "Temperament"){
-        ggplot(breedWinStats, aes(Temperament, Frequency)) + geom_col() + theme_hc() + theme(plot.background = element_rect(fill = "#E6E6FA")) + theme(axis.text.x = element_text(angle = 60, hjust = 1))
+        ggplot(breedWinStats, aes(Temperament, Frequency)) + 
+               geom_col() + 
+               theme_hc() + 
+               theme(plot.background = element_rect(fill = "#E6E6FA")) + 
+               theme(axis.text.x = element_text(angle = 60, hjust = 1))
       }
       else {
-        ggplot(dogGroupUnique, aes(Group, Frequency)) + geom_col() + theme_hc() + theme(plot.background = element_rect(fill = "#E6E6FA")) + theme(axis.text.x = element_text(angle = 60, hjust = 1))
+        ggplot(dogGroupUnique, aes(Group, Frequency)) + 
+               geom_col() + 
+               theme_hc() + 
+               theme(plot.background = element_rect(fill = "#E6E6FA")) + 
+               theme(axis.text.x = element_text(angle = 60, hjust = 1))
       }}
     }
-    
   )
   
+  #CREATING INTERACTIVE REGRESSION PLOT
+  
   output$Regression <- renderPlot({
+   
     if(input$regression == "Height"){
-      ggplot(breedWinStats, aes(Height, Frequency)) + geom_point() + theme_hc() + theme(plot.background = element_rect(fill = "#E6E6FA"))
+      ggplot(breedWinStats, aes(Height, Frequency)) + 
+             geom_point() + 
+             theme_hc() + 
+             theme(plot.background = element_rect(fill = "#E6E6FA"))
     }
     else {
       if(input$regression == "Weight"){
-        ggplot(breedWinStats, aes(Weight, Frequency)) + geom_point() + theme_hc() + theme(plot.background = element_rect(fill = "#E6E6FA"))
+        ggplot(breedWinStats, aes(Weight, Frequency)) + 
+               geom_point() + 
+               theme_hc() + 
+               theme(plot.background = element_rect(fill = "#E6E6FA"))
       }
       else {
-        ggplot(breedWinStats, aes(Lifespan, Frequency)) + geom_point() + theme_hc() + theme(plot.background = element_rect(fill = "#E6E6FA"))
+        ggplot(breedWinStats, aes(Lifespan, Frequency)) + 
+               geom_point() + 
+               theme_hc() + 
+               theme(plot.background = element_rect(fill = "#E6E6FA"))
       }
     }
   })
-
-
-
 }
-
-
-  
-
-    
--- ui.R
+++ ui.R
@@ -6,9 +6,12 @@ library(shinythemes)
 library(bslib)
 library(DT)
 
-# Creating Navigation bar
+#FLUID PAGE SET UP
+
 fluidPage(
 
+#CREATING NAVIGATION BAR AND ADDING APP THEME 
+  
 navbarPage("Westminster Dog Show Data",
            tags$style(HTML(".navbar.navbar-default {
                           background-color: #52247f !important;
@@ -25,75 +28,114 @@ navbarPage("Westminster Dog Show Data",
            }
                                }")),
           
-            theme = bs_theme(bootswatch = "flatly"),
-           
-           tabPanel("Map", 
-                    titlePanel("Westminster Dog Show Winners and their Origins"),
+          theme = bs_theme(bootswatch = "flatly"),
+          
+#CREATING FIRST TAB (INTERACTIVE MAP)
+          
+          tabPanel("Map", 
                     
-                    h5("Explore the origins of your favorite Westminster Dog Show Winners through this interactive map. Hover over the blue pins to preview the breed, and click into any pin to see a picture, the dog's origin, and group. Click and drag the map to see different areas, and zoom in to see more breeds."),
+                   titlePanel("Westminster Dog Show Winners and their Origins"),
+                    
+                   h5("Explore the origins of your favorite Westminster Dog Show Winners through this interactive map. Hover over the blue pins to preview the breed, and click into any pin to see a picture, the dog's origin, and group. Click and drag the map to see different areas, and zoom in to see more breeds."),
+                      
                     br(),
                     
-                    fluidRow(radioButtons(
-                      inputId = "mapTheme", "Map Theme:", choices = c("Dark mode", "Light mode"), selected = "Light mode"
+                   fluidRow(radioButtons(inputId = "mapTheme",
+                                                   "Map Theme:", 
+                                                   choices = c("Dark mode", "Light mode"), 
+                                                   selected = "Light mode"
                     ),
-                    leafletOutput("map", height="70vh")
+                    
+                   leafletOutput("map", 
+                                  height="70vh")
                     )),
            
-           tabPanel("Winners",
+#CREATING SECOND TAB (INTERACTIVE PLOT, WINNERS BY BREED)
+          
+          tabPanel("Winners",
                     titlePanel("Browse Winning Dogs by Breed"),
                     
                     h5("Explore the history of the winners of the Westminster Dog Show from 1907 to 2017. Click into the drop down, or type, to select which breeds you would like to see. Points on the plot will appear in the years that the selected breed(s) won."),
-                    br(),
-                    br(),
-                    br(),
+                      
+                      br(),
+                      br(),
+                      br(),
                     
-                     fluidRow(column(8, plotOutput("plot")),
-                              column(4, selectInput("dogBreed", "Filter by Breed",
-                                                    choices = finalDogData$Breed, 
-                                                    selected = finalDogData$Breed[9], 
-                                                    multiple = TRUE))
+                    fluidRow(column(8, plotOutput("plot")),
+                            column(4, selectInput("dogBreed", "Filter by Breed",
+                                                  choices = finalDogData$Breed, 
+                                                  selected = finalDogData$Breed[9], 
+                                                  multiple = TRUE))
            ),
            ),  
            
+#CREATING THIRD TAB (INTERACTIVE TABLE)
+          
            tabPanel("Breed Specifications",
                     titlePanel("Filter Winning Dogs by Breed Specifications"),
+                    
                     h5("Use filters under each column to sort through the winners by year of win, breed, height, weight, life expectancy and group."),
-                    br(),
-                    fluidRow(column(8,  DT::dataTableOutput("Dogs")),
+                    
+                      br(),
+                    
+                    fluidRow(column(8, DT::dataTableOutput("Dogs")),
                              column(4, downloadButton("downloadData", "Download")),
            ),
            ),
            
+#CREATING FOURTH TAB (COMPARISON PLOTS)
+          
            tabPanel("Comparison Plots",
                     titlePanel("Comparison Plots"),
+                   
                     h5("Compare winning frequency from 1907 to 2017 to three different Westminster Dog Show competitor statistics: origin, temperament, and group. Use the buttons below the x-axis to toggle between what statistic you are comparing to win frequency."),
+                    
                     plotOutput("Interactive"),
-                    br(),
+                      
+                      br(),
+                   
                     fluidRow(radioButtons(
-                      inputId = "barData", "X axis choices", choices = c("Origin", "Temperament", "Group"), selected = "Temperament")),
-                   br(),
+                      inputId = "barData", 
+                                "X axis choices", 
+                                choices = c("Origin", "Temperament", "Group"), 
+                                selected = "Temperament")),
+                        br(),
+                   
                     titlePanel("Physical Characteristic Comparisons"),
+                    
                     h5("Compare winning frequency from 1907 to 2017 to three physcial statistics: height, weight, and lifespan. Use the buttons below the x-axis to toggle between what statistic you are comparing to win frequency."),
+                    
                     plotOutput("Regression"),
+                    
                     fluidRow(radioButtons(
-                        inputId = "regression", "X axis choices", choices = c("Height", "Weight", "Lifespan"), selected = "Height")
+                        inputId = "regression", 
+                                  "X axis choices", 
+                                  choices = c("Height", "Weight", "Lifespan"), 
+                                  selected = "Height")
                     )),
            
+#CREATING FIFTH TAB (ABOUT PAGE)
+          
            tabPanel("About",
                     titlePanel("Our story"),
+                    
                     h4("The Westminster Kennel Club organization began in 1877 with the mission of enhancing dogs’ lives, celebrating their companionship, promoting responsible dog ownership, and preserving breeds. The Westminster Kennel Club Dog Show is an all-breed conformation competition that began in 1948, making it the longest nationally televised live dog show.  "),
+                    
                     h4("As dog lovers themselves, Grace, John, Jill, and Mary Camilla decided to bring to life a collection of data from both the American Kennel Club and the Westminster Kennel Club Dog Show.  "),
+                   
                     h4("We hope you all enjoy exploring the visualizations as much as we enjoyed creating them!"),
-                    br(),
-                    br(),
+                      
+                      br(),
+                      br(),
+                    
                     fluidRow(column(3, img(src = "IMG_6464.jpg",
-                                           width = "300px", height = "300px")),
+                                            width = "300px", height = "300px")),
                              column(3,  img(src = "thumbnail_IMG_8312.jpg",
                                             width = "300px", height = "300px")),
                              column(3, img(src = "thumbnail_IMG_E2596.jpg",
-                                       width = "300px", height = "300px")),
+                                            width = "300px", height = "300px")),
                              column(3, img(src = "thumbnail_IMG_8351.jpg",
-                                          width = "300px", height = "300px"))
+                                            width = "300px", height = "300px"))
                                      )),
 
 ))
